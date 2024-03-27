2b18532deb3f11aff3fc3a232cf2a314be81a63d
lofilabo <richardlofi@gmail.com>
2024-03-27 17:02:06 +0000

    New!! 4x4!!!
    First 4x4.  1234567890+- RTN

---
+++ Arduino_006_4x4/start_and_hope/start_and_hope.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {34, 36, 38, 40}; 
+byte colPins[COLS] = {42, 44, 46, 48}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
